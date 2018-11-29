e1176b25850f361c39a3520468104ead6f944e78
Sara Walker <sarawall1187@gmail.com>
2018-11-29 17:11:25 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -2,5 +2,29 @@
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+ ]
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] && 
+    board[combo[1]] == board[combo[2]] && position_taken?(board, combo[0]) 
+ end
+end
+
+def full?(board)
+  board[index].all? do |combo|
+    position_taken?(board, combo) == "X"
+  end
+end
+
 
 # Define your WIN_COMBINATIONS constant
